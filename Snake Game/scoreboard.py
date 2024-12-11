bf1ed3b430e3d8172413b8d9fc999f83414a74f6
A5hisa <themarch2546@gmail.com>
2024-12-11 14:19:23 +0700

    Update scoreboard.py

--- Snake Game/scoreboard.py
+++ Snake Game/scoreboard.py
@@ -5,7 +5,7 @@ ALIGN = "center"
 
 class Scoreboard(Turtle):
 
-    def __init__(self) :
+    def __init__(self):
         super().__init__()
         self.score = 0
         self.color("white")
@@ -14,7 +14,7 @@ class Scoreboard(Turtle):
         self.goto(0, 300)
         self.write(f"Score : {self.score}", False, align=ALIGN, font=FONT)
 
-    def score_plus(self) :
+    def score_plus(self):
         self.score += 1
         self.clear()
         self.write(f"Score : {self.score}", False, align=ALIGN, font=FONT)
