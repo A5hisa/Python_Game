53e1bd67caedef0067e8d9feb41ae9b7807cd1e0
Aussawin Saksawaddikul <86753641+A5hisa@users.noreply.github.com>
2024-12-11 16:50:04 +0700

    Update food.py

--- Snake Game/food.py
+++ Snake Game/food.py
@@ -4,8 +4,7 @@ import random
 class Food(Turtle):
     
     def __init__(self):
-        super().__init__()
-        self.shape("circle")
+        super().__init__(shape="circle)
         self.penup()
         self.shapesize(stretch_len=0.5, stretch_wid=0.5)
         self.color("red")
