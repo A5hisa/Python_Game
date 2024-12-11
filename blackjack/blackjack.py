4036552a5d3bf7a6bfb8bc36e5899cc8048d3189
A5hisa <themarch2546@gmail.com>
2024-12-11 14:12:16 +0700

    Update blackjack.py

--- blackjack/blackjack.py
+++ blackjack/blackjack.py
@@ -3,54 +3,54 @@ import art
 
 cards = [11, 2, 3, 4, 5, 6, 7, 8, 9, 10, 10, 10, 10]
 
-def display(optional) :
-    if optional == "y" :
+def display(optional):
+    if optional == "y":
         print(f"Your cards: {player}, current score: {sum(player)}")
         print(f"Computer's first card: {computer[0]}")
-    else :
+    else:
         print(f"Your final hand: {player}, final score: {sum(player)}")
         print(f"Computer's final hand: {computer}, final score {sum(computer)}")
 
-def player_draw() :
+def player_draw():
     player.append(random.choice(cards))
-    if player[len(player) - 1] == 11 :
-            if sum(player) > 21 :
+    if player[len(player) - 1] == 11:
+            if sum(player) > 21:
                 player.pop(len(player) - 1)
                 player.append(1)
 
-def computer_draw() :
-    while True :
-        if sum(computer) < 17 :
+def computer_draw():
+    while True:
+        if sum(computer) < 17:
             computer.append(random.choice(cards))
-            if computer[len(computer) - 1] == 11 :
+            if computer[len(computer) - 1] == 11:
                 if sum(computer) > 21 :
                     computer.pop(len(computer) - 1)
                     computer.append(1)
-        else :
+        else:
             break
 
-def check(last_player, last_computer) :
-    if sum(computer) > 21 :
+def check():
+    if sum(computer) > 21:
         print("You win 😎\n")
-    elif sum(player) == sum(computer) :
+    elif sum(player) == sum(computer):
         print("Draw 🤯\n")
-    elif sum(player) == 21 :
+    elif sum(player) == 21:
         print("Blackjack!, you win 😎\n")
-    elif sum(computer) == 21 :
+    elif sum(computer) == 21:
         print("Lose, computer has Blackjack 😭\n")
-    elif sum(computer) > sum(player) :
+    elif sum(computer) > sum(player):
         print("You lose 😭\n")
-    else :
+    else:
         print("You win 😎\n")
 
 
-while True :
+while True:
 
     player = []
     computer = []
 
     play = input("Do you want to play a game of Blackjack? Type 'y' or 'n' : ").lower()
-    if play == 'n' :
+    if play == 'n':
         break
 
     print("\n" * 10)
@@ -71,13 +71,13 @@ while True :
         draw_card = input("Type 'y' to get another card, type 'n' to pass : ").lower()
         if draw_card == "y" :
             player_draw()
-            if sum(player) > 21 :
+            if sum(player) > 21:
                 display("n")
                 print("You went over. You lose 😭\n")
                 another_card = False
-            else :
+            else:
                 display(draw_card)
-        else :
+        else:
             display("n")
-            check(player,computer)
+            check()
             another_card = False
