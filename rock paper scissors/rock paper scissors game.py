5d48e3831ce9806d17430e79e826dc554143e0b7
A5hisa <themarch2546@gmail.com>
2024-12-14 17:57:25 +0700

    Update rock paper scissors game.py

--- rock paper scissors/rock paper scissors game.py
+++ rock paper scissors/rock paper scissors game.py
@@ -25,21 +25,25 @@ scissors = '''
 '''
 option = [rock, paper, scissors]
 
-player = int(input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors.\n"))
+while True:
+    player = int(input("What do you choose? Type 0 for rock, 1 for paper or 2 for scissors. Type -1 for Exit.\n"))
 
-if player >= 3 or player < 0:
-    print("Invalid number, You lose!")
-else:
-    print(f"You chose\n {option[player]}")
-    computer = random.randint(0, 2)
-    print(f"Computer chose\n {option[computer]}")
-    if player == computer:
-        print("Draw!")
-    elif player == 0 and computer == 2:
-        print("You win!")
-    elif computer == 0 and player == 2:
-        print("You lose!")
-    elif player > computer:
-        print("You win!")
-    elif computer > player:
-        print("You lose!")
+    if player >= 3:
+        print("Invalid number, Try again!")
+    elif player == -1:
+        print("Exit")
+        break
+    else:
+        print(f"You chose\n {option[player]}")
+        computer = random.randint(0, 2)
+        print(f"Computer chose\n {option[computer]}")
+        if player == computer:
+            print("Draw!")
+        elif player == 0 and computer == 2:
+            print("You win!")
+        elif computer == 0 and player == 2:
+            print("You lose!")
+        elif player > computer:
+            print("You win!")
+        elif computer > player:
+            print("You lose!")
