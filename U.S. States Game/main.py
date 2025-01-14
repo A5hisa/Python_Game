f2e18554668ef19a1db20367b07df72734365a3f
A5hisa <themarch2546@gmail.com>
2025-01-14 21:38:44 +0700

    update requirements.txt

---
+++ U.S. States Game/main.py
@@ -0,0 +1,40 @@
+import turtle
+import pandas
+
+image = "blank_states_img.gif"
+FONT = ("Arial", 8, "normal")
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+screen.addshape(image)
+turtle.shape(image)
+
+text = turtle.Turtle()
+text.hideturtle()
+text.penup()
+
+data = pandas.read_csv("50_states.csv")
+states = data.state.to_list()
+title = "Guess the State"
+
+correct = 0
+correct_list = []
+
+while True:
+    answer = screen.textinput(title=title, prompt="What's another states's name?")
+
+    if answer == None:
+        break
+    if correct == 50:
+        break
+    if answer.lower() == "exit":
+        break
+
+    if answer.lower() not in correct_list:
+        for index, state in enumerate(states):
+            if answer.lower() == state.lower():
+                correct_list.append(answer.lower())
+                correct += 1
+                title = f"{correct}/50 States Correct"
+                text.goto(data.at[index,"x"], data.at[index, "y"])
+                text.write(state, font=FONT)
